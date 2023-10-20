6ff52ac063fc500d304a68b1b69a74968a7fc4af
leadpro456 <ravinduyasiska@gmail.com>
2023-10-20 09:55:19 +0530

    Receipt Print

--- src/containers/Common/ConfirmationPage.js
+++ src/containers/Common/ConfirmationPage.js
@@ -3,7 +3,10 @@ import { Box, Divider, Button, TextField, GlobalStyles } from "@mui/material";
 import Barcode from "react-barcode";
 import { toJpeg } from "html-to-image";
 import { useLocation } from "react-router-dom";
+import { jsPDF } from "jspdf";
+import html2canvas from "html2canvas";
 import emailjs from "@emailjs/browser";
+import ReceiptTemplate from "../../components/Documents/ReceiptTemplate";
 
 function ConfirmationPage() {
   const [senderEmail, setSendersEmail] = useState("");
@@ -44,23 +47,40 @@ function ConfirmationPage() {
       );
   };
 
-  const printReceipt = () => {
-    const node = document.createElement("div");
-    node.style.display = "flex";
-    node.style.justifyContent = "center";
-    node.style.alignItems = "center";
+  const generatePDF = async () => {
+    const receiptElement = document.getElementById("pdf-receipt");
 
-    node.style.backgroundColor = "white";
-    node.appendChild(receiptRef.current.cloneNode(true));
+    // Temporarily make the content visible for html2canvas
+    receiptElement.style.display = "block";
 
-    toJpeg(node, { quality: 0.95, width: 500, height: 700 }).then(function (
-      dataUrl
-    ) {
-      var link = document.createElement("a");
-      link.download = "receipt.jpeg";
-      link.href = dataUrl;
-      link.click();
+    const canvas = await html2canvas(receiptElement);
+
+    // Hide the content again after the snapshot is taken
+    receiptElement.style.display = "none";
+
+    const imgData = canvas.toDataURL("image/jpeg", 1.0); // using JPEG format (max quality)
+
+    // Define custom PDF size
+    const pdfWidth = 105; // width in mm (A4 size)
+    const pdfHeight = 150; // height in mm (A4 size)
+
+    const pdf = new jsPDF({
+      orientation: "portrait",
+      unit: "mm",
+      format: [pdfWidth, pdfHeight],
     });
+
+    // Calculate scaling factors for zoom out effect
+    const zoomFactor = 1; // Adjust this value to zoom in or out
+    const imageWidth = ((canvas.width * 25.4) / 96) * zoomFactor; // Convert pixels to mm and apply zoom factor
+    const imageHeight = ((canvas.height * 25.4) / 96) * zoomFactor; // Convert pixels to mm and apply zoom factor
+
+    // Calculate x and y offsets to center the image on the page
+    const offsetX = (pdfWidth - imageWidth) / 2;
+    const offsetY = (pdfHeight - imageHeight) / 2;
+
+    pdf.addImage(imgData, "JPEG", offsetX, offsetY, imageWidth, imageHeight);
+    pdf.save("receipt.pdf");
   };
 
   const printBarCode = () => {
@@ -317,9 +337,18 @@ function ConfirmationPage() {
             }}
           />
 
+          <div id="pdf-receipt" style={{ display: "none" }}>
+            <ReceiptTemplate
+              mailId={mailId}
+              securityNumber={securityNumber}
+              cost={cost}
+              type={type}
+            />
+          </div>
+
           <Button
             variant="contained"
-            onClick={printReceipt}
+            onClick={generatePDF}
             sx={{
               backgroundColor: "#852318",
               color: "white",
