628e9796db8eb1f6de99cdb0d0be824d5d98cc29
sahiru <sahiruwrcg@gmail.com>
2023-10-29 19:19:55 +0530

    add route error handling

--- src/containers/Supervisor/AddRoute.js
+++ src/containers/Supervisor/AddRoute.js
@@ -1,37 +1,16 @@
 import React, { useState, useEffect, useRef } from "react";
-import { db } from "../../config/firebase";
-import {
-  collection,
-  query,
-  where,
-  getDocs,
-  doc,
-  getDoc,
-} from "firebase/firestore";
+import { auth } from "../../config/firebase";
+
 import {
   Box,
-  IconButton,
-  Typography,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  useTheme,
   Button,
   LinearProgress, // Import LinearProgress for the progress bar
 } from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import ExpandLessIcon from "@mui/icons-material/ExpandLess";
-import Barcode from "react-barcode";
-import { toJpeg } from "html-to-image";
-import LoadingScreen from "../Common/LoadingScreen";
+
 import { fetchSupervisorPostOfficeId } from "../../data/databaseFunctions";
 
 const AddRoute = () => {
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const [po, setpo] = useState("");
 
   useEffect(() => {
@@ -47,28 +26,33 @@ const AddRoute = () => {
     console.log("clicked");
     setLoading(true);
 
-    await fetch(`${process.env.REACT_APP_SERVER_LINK}/route?po=${po}`)
+    let token = await auth.currentUser.getIdToken();
+
+    const headers = new Headers({
+      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
+    });
+
+    const requestOptions = {
+      method: "GET",
+      headers: headers,
+    };
+
+    await fetch(
+      `${process.env.REACT_APP_SERVER_LINK}/route?po=${po}`,
+      requestOptions
+    )
       .then((res) => res.json())
       .then((data) => {
         console.log(data);
 
-        if (data.status) {
-          setError(false);
-        }
-        else  {
-          setError(true);
-        }
-      }).catch((err) => {
+        alert(data.desc);
+      })
+      .catch((err) => {
         console.log(err);
-        setError(true);
+        alert("Error in adding routes");
       });
 
-    if (error){
-      alert("Error in adding routes");
-    }
-    else {
-      alert("Routes added successfully");
-    }
     setLoading(false);
   };
 
@@ -91,14 +75,16 @@ const AddRoute = () => {
       </Box>
 
       <div style={{ width: "40%", marginTop: 40 }}>
-        {loading && <LinearProgress
-          variant= "query"
-          style={{
-            backgroundImage:
-              "linear-gradient(90deg, #4CAF50 25%, transparent 25%, transparent 75%, #4CAF50 75%)",
-            backgroundSize: "100px 100%",
-          }}
-        />}
+        {loading && (
+          <LinearProgress
+            variant="query"
+            style={{
+              backgroundImage:
+                "linear-gradient(90deg, #4CAF50 25%, transparent 25%, transparent 75%, #4CAF50 75%)",
+              backgroundSize: "100px 100%",
+            }}
+          />
+        )}
       </div>
 
       <Button
